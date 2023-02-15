8bfb8333fb202c5899e6c62dc414a23fe5abb0b9
Assaad <assaadharbgmail.com>
2023-02-16 01:42:24 +0200

    update

--- packages/js/general/general.js
+++ packages/js/general/general.js
@@ -1,3 +1,7 @@
+$('form.disable-on-submit').on('submit',function(){
+    $(this).find('input[type=submit]').prop('disabled',true);
+})
+
 function TWAGENERAL_card_valueChange(elem) {
     $('input[name="'+$(elem).prop('name')+'"]').parents('.twa-card').removeClass('selected');
     $('input[name="'+$(elem).prop('name')+'"]:checked').parents('.twa-card').addClass('selected');
